9ae5a42e28720ca47390256a2170d05206d5c76d
Дилюк <@DESKTOP-5SJPVS1>
2024-04-21 12:11:40 +0300

    Добавьте файлы проекта.

---
+++ Server1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.GetEncoding(866);
+            Console.WriteLine("Однопоточный сервер запущен");
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 8888);
+
+
+            Socket sock = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+
+                sock.Bind(ipEndPoint);
+
+                sock.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Слушаем, порт {0}", ipEndPoint);
+
+
+                    Socket s = sock.Accept();
+
+                    string data = null;
+
+                    byte[] bytes = new byte[1024];
+
+                    int bytesCount = s.Receive(bytes);
+
+                    data += Encoding.UTF8.GetString(bytes, 0, bytesCount);
+
+                    Console.Write("Данные от клиента: " + data + "\n\n");
+
+                    string reply = "Query size: " + data.Length.ToString() + " chars";
+
+                    byte[] msg = Encoding.UTF8.GetBytes(reply);
+
+                    s.Send(msg);
+                    if (data.IndexOf("<TheEnd>") > -1)
+                    {
+                        Console.WriteLine("Соединение завершено.");
+                        break;
+                    }
+                    s.Shutdown(SocketShutdown.Both);
+                    s.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
